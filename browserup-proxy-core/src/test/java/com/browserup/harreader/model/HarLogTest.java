d9fca0a0c512073b0d03c3f971204ae483bab42a
Valery Yatsynovich <valery_yatsynovich@epam.com>
2022-05-26 22:52:51 +0300

    Add filtering method: `HarLog#findMostRecentEntry()`

--- browserup-proxy-core/src/test/java/com/browserup/harreader/model/HarLogTest.java
+++ browserup-proxy-core/src/test/java/com/browserup/harreader/model/HarLogTest.java
@@ -1,10 +1,16 @@
 package com.browserup.harreader.model;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
 import java.time.Instant;
 import java.util.Date;
 import java.util.Optional;
 import java.util.regex.Pattern;
-import org.junit.Assert;
+
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -19,51 +25,51 @@ public class HarLogTest extends AbstractMapperTest<HarLog> {
     @Test
     public void testVersion() {
         HarLog log = new HarLog();
-        Assert.assertEquals(EXPECTED_DEFAULT_VERSION, log.getVersion());
+        assertEquals(EXPECTED_DEFAULT_VERSION, log.getVersion());
 
         log.setVersion("1.2");
-        Assert.assertEquals("1.2", log.getVersion());
+        assertEquals("1.2", log.getVersion());
 
         log.setVersion(null);
-        Assert.assertEquals(EXPECTED_DEFAULT_VERSION, log.getVersion());
+        assertEquals(EXPECTED_DEFAULT_VERSION, log.getVersion());
 
         log.setVersion("");
-        Assert.assertEquals(EXPECTED_DEFAULT_VERSION, log.getVersion());
+        assertEquals(EXPECTED_DEFAULT_VERSION, log.getVersion());
 
         log.setVersion("  ");
-        Assert.assertEquals(EXPECTED_DEFAULT_VERSION, log.getVersion());
+        assertEquals(EXPECTED_DEFAULT_VERSION, log.getVersion());
     }
 
     @Test
     public void testPages() {
         HarLog log = new HarLog();
-        Assert.assertEquals(EXPECTED_PAGES_LIST, log.getPages());
+        assertEquals(EXPECTED_PAGES_LIST, log.getPages());
 
         log.setPages(null);
-        Assert.assertEquals(EXPECTED_PAGES_LIST, log.getPages());
+        assertEquals(EXPECTED_PAGES_LIST, log.getPages());
     }
 
     @Test
     public void testEntries() {
         HarLog log = new HarLog();
-        Assert.assertEquals(EXPECTED_ENTRIES_LIST, log.getEntries());
+        assertEquals(EXPECTED_ENTRIES_LIST, log.getEntries());
 
         log.setEntries(null);
-        Assert.assertEquals(EXPECTED_ENTRIES_LIST, log.getEntries());
+        assertEquals(EXPECTED_ENTRIES_LIST, log.getEntries());
     }
 
     @Test
     public void testCreatorNull() {
         HarLog log = new HarLog();
         log.setCreator(null);
-        Assert.assertNotNull(log.getCreator());
+        assertNotNull(log.getCreator());
     }
 
     @Test
     public void testBrowserNull() {
         HarLog log = new HarLog();
         log.setBrowser(null);
-        Assert.assertNull(log.getBrowser());
+        assertNull(log.getBrowser());
     }
 
     @Test
@@ -80,7 +86,7 @@ public class HarLogTest extends AbstractMapperTest<HarLog> {
             log.getEntries().add(entry);
         }
 
-        Assert.assertEquals("Expected to find all entries",
+        assertEquals("Expected to find all entries",
             log.findEntries(urlPattern).size(), entriesNumber);
     }
 
@@ -98,8 +104,22 @@ public class HarLogTest extends AbstractMapperTest<HarLog> {
 
         log.getEntries().add(entry);
 
-        Assert.assertFalse("Expected to get empty entry",
+        assertFalse("Expected to get empty entry",
             log.findMostRecentEntry(urlPattern).isPresent());
+    }
+
+    @Test
+    public void testFindEntryReturnsMostRecentEntryFilteredByUrl() {
+        String url = "http://abc.com";
+        Date firstDate = Date.from(Instant.ofEpochSecond(1000));
+        Date secondDate = Date.from(Instant.ofEpochSecond(2000));
+
+        HarLog log = createHarLog(url, firstDate, secondDate);
+
+        Optional<HarEntry> entry = log.findMostRecentEntry(Pattern.compile("^http://abc\\.com?"));
+        assertTrue("Expected to find entry", entry.isPresent());
+        assertEquals("Expected to find the most recent entry",
+            entry.get().getStartedDateTime(), secondDate);
     }
 
     @Test
@@ -108,6 +128,30 @@ public class HarLogTest extends AbstractMapperTest<HarLog> {
         Date firstDate = Date.from(Instant.ofEpochSecond(1000));
         Date secondDate = Date.from(Instant.ofEpochSecond(2000));
 
+        HarLog log = createHarLog(url, firstDate, secondDate);
+
+        Optional<HarEntry> entry = log.findMostRecentEntry();
+        assertTrue("Expected to find entry", entry.isPresent());
+        assertEquals("Expected to find the most recent entry",
+            entry.get().getStartedDateTime(), secondDate);
+    }
+
+    @Override
+    public void testMapping() {
+        HarLog log = map("{\"creator\": {}, \"browser\": {}, \"comment\": \"My comment\"}", HarLog.class);
+
+        assertEquals(EXPECTED_DEFAULT_VERSION, log.getVersion());
+        assertNotNull(log.getCreator());
+        assertNotNull(log.getBrowser());
+        assertEquals(EXPECTED_PAGES_LIST, log.getPages());
+        assertEquals(EXPECTED_ENTRIES_LIST, log.getEntries());
+        assertEquals("My comment", log.getComment());
+
+        log = map(UNKNOWN_PROPERTY, HarLog.class);
+        assertNotNull(log);
+    }
+
+    private HarLog createHarLog(String url, Date firstDate, Date secondDate) {
         HarLog log = new HarLog();
 
         HarEntry entry1 = new HarEntry();
@@ -124,25 +168,6 @@ public class HarLogTest extends AbstractMapperTest<HarLog> {
 
         log.getEntries().add(entry1);
         log.getEntries().add(entry2);
-
-        Optional<HarEntry> entry = log.findMostRecentEntry(Pattern.compile("^http://abc\\.com?"));
-        Assert.assertTrue("Expected to find entry", entry.isPresent());
-        Assert.assertEquals("Expected to find the most recent entry",
-            entry.get().getStartedDateTime(), secondDate);
-    }
-
-    @Override
-    public void testMapping() {
-        HarLog log = map("{\"creator\": {}, \"browser\": {}, \"comment\": \"My comment\"}", HarLog.class);
-
-        Assert.assertEquals(EXPECTED_DEFAULT_VERSION, log.getVersion());
-        Assert.assertNotNull(log.getCreator());
-        Assert.assertNotNull(log.getBrowser());
-        Assert.assertEquals(EXPECTED_PAGES_LIST, log.getPages());
-        Assert.assertEquals(EXPECTED_ENTRIES_LIST, log.getEntries());
-        Assert.assertEquals("My comment", log.getComment());
-
-        log = map(UNKNOWN_PROPERTY, HarLog.class);
-        Assert.assertNotNull(log);
+        return log;
     }
 }
