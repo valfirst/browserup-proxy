6a919f2c8dcd011dae097a16b01a60023889ba37
Valery Yatsynovich <valery_yatsynovich@epam.com>
2022-05-30 18:25:14 +0300

    Add ability to convert HAR to byte array

--- browserup-proxy-core/src/test/java/com/browserup/harreader/model/HarTest.java
+++ browserup-proxy-core/src/test/java/com/browserup/harreader/model/HarTest.java
@@ -4,9 +4,11 @@ import static org.junit.Assert.assertEquals;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 import com.browserup.harreader.HarReader;
 import com.browserup.harreader.HarReaderException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -42,6 +44,16 @@ public class HarTest extends AbstractMapperTest<Har>{
         har.setLog(new HarLog());
         Har deepCopy = har.deepCopy();
         assertEquals(har, deepCopy);
+    }
+
+    @Test
+    public void testAsBytes() throws HarReaderException, IOException {
+        File harFile = new File("src/test/resources/sstoehr.har");
+        ObjectMapper mapper = new ObjectMapper();
+        Har expected = mapper.readValue(harFile, Har.class);
+
+        Har har = new HarReader().readFromFile(harFile);
+        assertEquals(mapper.writeValueAsString(expected), new String(har.asBytes(), StandardCharsets.UTF_8));
     }
 
 }
