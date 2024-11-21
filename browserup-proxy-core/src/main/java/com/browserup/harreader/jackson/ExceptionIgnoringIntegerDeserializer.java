083c18d14aa3a3aa602f69ab92139c05e7289ab6
Uladzislau Arlouski <uarlouski@gmail.com>
2024-11-21 15:41:59 +0200

    Remove deprecated HAR reader APIs (#442)

--- browserup-proxy-core/src/main/java/com/browserup/harreader/jackson/ExceptionIgnoringIntegerDeserializer.java
+++
@@ -1,9 +0,0 @@
-package com.browserup.harreader.jackson;
-
-/**
- * @deprecated Use {@link de.sstoehr.harreader.jackson.ExceptionIgnoringIntegerDeserializer}
- */
-@Deprecated
-public class ExceptionIgnoringIntegerDeserializer extends de.sstoehr.harreader.jackson.ExceptionIgnoringIntegerDeserializer {
-
-}
