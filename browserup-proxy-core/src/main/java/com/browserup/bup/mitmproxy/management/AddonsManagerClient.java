d66813fd10cbe3981898495ce636e6f0596b0b71
Valery Yatsynovich <valery_yatsynovich@epam.com>
2024-11-21 19:51:40 +0300

    Remove unused import

--- browserup-proxy-core/src/main/java/com/browserup/bup/mitmproxy/management/AddonsManagerClient.java
+++ browserup-proxy-core/src/main/java/com/browserup/bup/mitmproxy/management/AddonsManagerClient.java
@@ -11,7 +11,6 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
-import java.util.Objects;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
